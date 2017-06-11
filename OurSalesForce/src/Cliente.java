169e1ba4f5efe7af3caa1ced0c867912c2051d34
Pongelupe <pongelupe1998@gmail.com>
2017-06-11 15:26:41 -0300

    Pessoa and Cliente classes were developed

--- OurSalesForce/src/Cliente.java
+++ OurSalesForce/src/Cliente.java
@@ -1,4 +1,78 @@
+import java.util.ArrayList;
 
-public class Cliente {
+public class Cliente extends Pessoa {
+
+	private String numeroConta;
+	private double saldo;
+	private Vendedor vendedor;
+	private ArrayList<Produto> items;
+
+	public Cliente(String documento, String nome, String numeroConta, double saldo, Vendedor vendedor) {
+		super(documento, nome);
+		setNumeroConta(numeroConta);
+		setSaldo(saldo);
+		setVendedor(vendedor);
+		this.items = new ArrayList<>();
+	}
+
+	public Cliente(String documento, String numeroConta, Vendedor vendedor) {
+		super(documento);
+		setNumeroConta(numeroConta);
+		setSaldo(0);
+		setVendedor(vendedor);
+		this.items = new ArrayList<>();
+	}
+
+	public String getNumeroConta() {
+		return numeroConta;
+	}
+
+	public void setNumeroConta(String numeroConta) {
+		this.numeroConta = numeroConta;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	public Vendedor getVendedor() {
+		return vendedor;
+	}
+
+	public void setVendedor(Vendedor vendedor) {
+		this.vendedor = vendedor;
+	}
+
+	public ArrayList<Produto> getItems() {
+		return items;
+	}
+
+	public void comprar(Produto p) {
+		if (getVendedor().vender(p)) {
+			getItems().add(p);
+		}
+	}
+
+	public String produtosComprados() {
+		if (getItems().isEmpty())
+			return "Sem produtos!";
+		else {
+			String produtos = "";
+			for (Produto p : getItems()) {
+				produtos += p.toString() + "\n";
+			}
+			return produtos;
+		}
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + "\nNúmero da conta: " + getNumeroConta() + "\nVendedor: " + getVendedor().toString()
+				+ "\nProdutos comprados: " + produtosComprados();
+	}
 
 }
--- OurSalesForce/src/Pessoa.java
+++ OurSalesForce/src/Pessoa.java
@@ -1,4 +1,37 @@
 
 public abstract class Pessoa {
 
+	private String nome;
+	private String documento;
+
+	public Pessoa(String documento) {
+		this(documento, "Shulambs");
+	}
+
+	public Pessoa(String documento, String nome) {
+		setDocumento(documento);
+		setNome(nome);
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDocumento() {
+		return documento;
+	}
+
+	public void setDocumento(String documento) {
+		this.documento = documento;
+	}
+
+	@Override
+	public String toString() {
+		return "Nome: " + getNome() + "\nNúmero do documento: " + getDocumento();
+	}
+
 }
